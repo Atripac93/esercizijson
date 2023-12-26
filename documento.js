5253f75be3a5c3c70ac9fb11bbc81c83ab48e384
Carlo Atripaldi <carloatrica@gmail.com>
2023-12-26 18:47:09 +0100

    Update documento.js

--- documento.js
+++ documento.js
@@ -1,5 +1,8 @@
-const ses = function(){
-    document.querySelector(".si").innerHTML="sosososo"
-}
-const button = document.querySelector(".mybutton")
-button.addEventListener("click",ses)
+const img = document.querySelector("#img");
+
+const fetchCanzoni = async () => {
+  const data = await fetch("https://dog.ceo/api/breeds/image/random");
+  const dataJson = await data.json();
+  img.src = dataJson.message;
+};
+fetchCanzoni();
