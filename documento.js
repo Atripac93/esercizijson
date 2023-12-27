fb9587fb18ef665a1cdff1c87c26f305a9421b94
Carlo Atripaldi <carloatrica@gmail.com>
2023-12-27 15:47:07 +0100

    Update documento.js

--- documento.js
+++ documento.js
@@ -1,8 +1,15 @@
-const img = document.querySelector("#img");
-
-const fetchCanzoni = async () => {
-  const data = await fetch("https://dog.ceo/api/breeds/image/random");
-  const dataJson = await data.json();
-  img.src = dataJson.message;
+const fetchEminem = () => {
+  fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      const eminems = data.eminems;
+      const img = document.querySelector(".img");
+      for (const eminem of eminems) {
+        img.innerHTML += `<div> <input type="checkbox" id="eminem_${eminem.id}"/>
+        <label for=eminem_${eminem.id}">${eminem.img}</label> </div>`;
+      }
+    });
 };
-fetchCanzoni();
