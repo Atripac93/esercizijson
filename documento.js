fc8dd1247b2b05c02691f30ab148d7d64a42b0c2
Carlo Atripaldi <carloatrica@gmail.com>
2023-12-27 17:29:35 +0100

    Update documento.js

--- documento.js
+++ documento.js
@@ -5,11 +5,31 @@ const fetchEminem = () => {
       return response.json();
     })
     .then((data) => {
-      const eminems = data.eminems;
+      console.log(data);
+      const eminems = data.data;
       const img = document.querySelector(".img");
       for (const eminem of eminems) {
         img.innerHTML += `<div> <input type="checkbox" id="eminem_${eminem.id}"/>
-        <label for=eminem_${eminem.id}">${eminem.img}</label> </div>`;
+        <label for=eminem_${eminem.id}">${eminem.album.title}</label> </div>`;
       }
-    });
+    }).catch;
 };
+fetchEminem();
+
+const fetchMetallica = () => {
+  fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica")
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const metallicas = data.data;
+      const img1 = document.querySelector(".img1");
+      for (const metallica of metallicas) {
+        img1.innerHTML += `<div> <input type="checkbox" id="eminem_${metallica.id}"/>
+        <label for=eminem_${metallica.id}">${metallica.album.title}</label> </div>`;
+      }
+    }).catch;
+};
+fetchMetallica();
